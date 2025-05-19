c9d6e614e326fe2cb27349a116fc73cad5735d2a
Nishi Kanta Paul <nishi.kanta@brainstation-23.com>
2025-05-19 19:51:57 +0600

    Implemented Sign-in,Sign-Up and Logout

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  console.log("Middleware running for:", req.nextUrl.pathname); // Add this line
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
